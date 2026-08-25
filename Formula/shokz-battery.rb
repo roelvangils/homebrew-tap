1828b4f0a6ee99e47982c0b8ee48e53bd9c0b797
Roel Van Gils <roelvangils@gmail.com>
2026-08-25 19:33:14 +0200

    shokz-battery 1.3.0

--- Formula/shokz-battery.rb
+++ Formula/shokz-battery.rb
@@ -1,8 +1,8 @@
 class ShokzBattery < Formula
   desc "Check your Shokz headset battery level from the command line"
   homepage "https://github.com/roelvangils/shokz-battery"
-  url "https://github.com/roelvangils/shokz-battery/archive/refs/tags/v1.2.2.tar.gz"
-  sha256 "d77a822b6b877acb91f95ac34e3e7f15239b3caec232d2282a607753c51def79"
+  url "https://github.com/roelvangils/shokz-battery/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "fece1ff7e121ffe3e3afebab39f8484b2ddeccc827de7d0771e64f80abede123"
   license "MIT"
 
   depends_on :macos
