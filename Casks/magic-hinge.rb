0f0d506b1273e397adde8c08bb93d7e41d5ffeb8
Roel Van Gils <roel@elevenways.be>
2026-09-12 23:01:45 +0200

    Update Magic Hinge to 0.9.1 Beta with new native icon

--- Casks/magic-hinge.rb
+++ Casks/magic-hinge.rb
@@ -1,9 +1,9 @@
 cask "magic-hinge" do
-  version "0.9.0"
-  sha256 "338bc3cb7e3a673025dc978ab6761c7ca16934b86f0d9d777e5fbf8907cf9606"
+  version "0.9.1"
+  sha256 "ae83350d7ef31a4b82344393318426052d5bf30bdde8c0ca7bde25f44d4b58ac"
 
   url "https://github.com/roelvangils/magic-hinge/releases/download/v#{version}/Magic-Hinge-#{version}.dmg"
-  name "Magic Hinge 0.9 Beta"
+  name "Magic Hinge 0.9.1 Beta"
   desc "Frosted-glass desktop effect controlled by your MacBook hinge"
   homepage "https://roelvangils.github.io/magic-hinge/"
 
